5a510a090b40bbaa32674ccd5ce6f21a8a23092f
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-29 18:33:17 -0400

    there are some problems with babel just going to restart project

--- src/SOSfrontend/app/routes/index.jsx
+++ src/SOSfrontend/app/routes/index.jsx
@@ -1,9 +1,12 @@
+import { Button } from 'mui/material/Button';
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to SOSbot</h1>
       <p>A discord bot that helps people get in contact with SOS helpers</p>
-      <a>slack.io/sosbot</a>
+      <a href="">slack.io/sosbot</a>
+      <Button variant='contained'>Github</Button>
     </div>
   );
 }
