1bac8a07046cc123767f0b2f6d9d284cb7c622be
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-29 11:52:47 -0400

    Update index.jsx

--- src/SOSfrontend/app/routes/index.jsx
+++ src/SOSfrontend/app/routes/index.jsx
@@ -2,7 +2,8 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to SOSbot</h1>
-      
+      <p>A discord bot that helps people get in contact with SOS helpers</p>
+      <a>slack.io/sosbot</a>
     </div>
   );
 }
